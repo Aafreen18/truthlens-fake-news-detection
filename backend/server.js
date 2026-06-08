fe885523a6915b1ad2a5eaabcc3c9802da8d7c8d
Aafreen Parveen <aafreenparveen@Aafreens-MacBook-Air.local>
2026-06-08 06:16:16 +0530

    added backend api

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,19 @@ import predictRouter from "./routes/predict.js"
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: "*"
+}))
+
 app.use(express.json())
 app.use(predictRouter)
 
 app.get("/", (req, res) => {
-  res.send("Fake News Predictor API running")
+    res.send("Fake News Predictor API running")
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
+    console.log(`Server listening on port ${PORT}`)
 })
